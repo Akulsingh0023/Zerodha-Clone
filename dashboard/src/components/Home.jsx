140d2976ee5ae6502ece462cf6e341271477c5aa
Delta Student <akul212006@gmail.com>
2026-03-20 23:56:04 +0530

    Removed dashboard sidebar component and updated home layout

--- dashboard/src/components/Home.jsx
+++ dashboard/src/components/Home.jsx
@@ -2,14 +2,12 @@ import React, { useState } from "react";
 
 import Dashboard from "./Dashboard";
 import TopBar from "./TopBar";
-import DashboardSidebar from "./DashboardSidebar";
 
 const Home = () => {
   const [watchlistOpen, setWatchlistOpen] = useState(false);
 
   return (
     <>
-      <DashboardSidebar />
       <TopBar
         onToggleWatchlist={() => setWatchlistOpen((prev) => !prev)}
         isWatchlistOpen={watchlistOpen}
