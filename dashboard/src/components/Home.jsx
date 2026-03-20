9f20aa8ed2a427aae5af560f810b18dcacb2773f
Delta Student <akul212006@gmail.com>
2026-03-20 23:32:35 +0530

    Updated home layout and added dashboard sidebar component

--- dashboard/src/components/Home.jsx
+++ dashboard/src/components/Home.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 
 import Dashboard from "./Dashboard";
 import TopBar from "./TopBar";
+import DashboardSidebar from "./DashboardSidebar";
 
 const Home = () => {
   const [watchlistOpen, setWatchlistOpen] = useState(false);
 
   return (
     <>
+      <DashboardSidebar />
       <TopBar
         onToggleWatchlist={() => setWatchlistOpen((prev) => !prev)}
         isWatchlistOpen={watchlistOpen}
