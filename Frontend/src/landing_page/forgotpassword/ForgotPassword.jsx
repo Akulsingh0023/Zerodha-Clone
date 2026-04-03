f71544aa7d96db7bfb51888638d1c72dc22a30fd
Delta Student <akul212006@gmail.com>
2026-04-04 00:33:02 +0530

    Updated forget feature

--- Frontend/src/landing_page/forgotpassword/ForgotPassword.jsx
+++ Frontend/src/landing_page/forgotpassword/ForgotPassword.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
-import { API } from "../../config";
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
@@ -15,7 +14,7 @@ export default function ForgotPassword() {
 
     try {
       const res = await axios.post(
-        `${API}/api/auth/forgot-password`,
+        `${import.meta.env.VITE_API_URL}/api/auth/forgot-password`,
         { email },
         { timeout: 10000 }
       );
